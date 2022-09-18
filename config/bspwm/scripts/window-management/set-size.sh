d800cde9af949bd8ae9ac9343334eb9baf68be32
alex <alexnlarsen@gmail.com>
2022-09-18 20:22:25 +0200

    Update wm settings for ultrawide monitor

--- config/bspwm/scripts/window-management/set-size.sh
+++ config/bspwm/scripts/window-management/set-size.sh
@@ -1,10 +1,14 @@
 #!/bin/bash 
 
 echo "Please click the window"
-win=$(xwininfo)
-
-winId=$(echo "$win" | grep "Window id:" | grep -o "0x[^ ]\+")
-echo "Got window with ID $winId"
+winId="$3"
+if [ -z "$winId" ]; then 
+  win=$(xwininfo)
+  winId=$(echo "$win" | grep "Window id:" | grep -o "0x[^ ]\+")
+  echo "Got window with ID $winId"
+else 
+  win=$(xwininfo -id $winId)
+fi
 
 width=$(echo "$win" | grep "Width:" | cut -d: -f2)
 height=$(echo "$win" | grep "Height:" | cut -d: -f2)
@@ -16,6 +20,7 @@ newHeight=$2
 
 echo "Resizing to $newWidth x $newHeight"
 
-bspc node $winId --resize right $((newWidth - width)) 0
-bspc node $winId --resize bottom 0 $((newHeight - height))
+bspc node "$winId" --resize right $((newWidth - width)) 0
+bspc node "$winId" --resize bottom 0 $((newHeight - height))
 
+echo "New size: $(bspc query -n $winId -T | yq '.client.tiledRectangle.width + "x" + .client.tiledRectangle.height')"
