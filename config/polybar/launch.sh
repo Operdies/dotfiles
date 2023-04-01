6b56a1b588c2d69a7edfc9e993c8e33be2aaf131
alex <alexnlarsen@gmail.com>
2023-04-01 23:55:38 +0200

    Configure virtual displays

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-killall -q polybar
+case $1 in
+left | right)
+	polybar "$1" -c ~/.config/polybar/config.ini &
+	;;
+kill)
+	# Terminate already running bar instances
+	killall -q polybar
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+	;;
+*)
+	MONITOR="$1" polybar generic -c ~/.config/polybar/config.ini &
+	;;
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch Polybar
-polybar right -c ~/.config/polybar/config.ini &
-polybar left -c ~/.config/polybar/config.ini &
+esac
