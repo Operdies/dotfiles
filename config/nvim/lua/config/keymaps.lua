fa7ce36200a939869ca582becb5ec3f6c5f947c5
alex <alexnlarsen@gmail.com>
2023-09-05 19:27:34 +0200

    insert mode navigation

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -6,10 +6,23 @@
 pcall(vim.keymap.del, "n", "<leader>gG")
 
 local map = vim.keymap.set
-local opts = { silent = true }
+local opts = { silent = true, remap = true, noremap = false }
 
 
 -- Insert blank line before/after cursor and restore position
 map("n", "[<space>", "mlO<Esc>`l", opts)
 map("n", "]<space>", "mlo<Esc>`l", opts)
+
+
+map("i", "<M-h>", '<C-o>b', opts)
+map("i", "<M-l>", '<C-o>e', opts)
+
+map("i", "<C-h>", '<C-o>h', opts)
+map('i', '<C-l>', '<C-o>l', opts)
+
+map("i", "<M-C-h>", '<C-o>B', opts)
+map("i", "<M-C-l>", '<C-o>E', opts)
+
+map('i', '<M-C-j>', '<C-o>gj', opts)
+map('i', '<M-C-k>', '<C-o>gk', opts)
 
