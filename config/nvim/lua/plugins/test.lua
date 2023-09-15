40bab217c02cd0454903aa8d4ba90eb4cc542e11
Alexander Larsen <alexnlarsen@gmail.com>
2023-09-15 10:27:04 +0200

    oopsie

--- config/nvim/lua/plugins/test.lua
+++ config/nvim/lua/plugins/test.lua
@@ -1,5 +1,18 @@
 return {
 	{
+		"mfussenegger/nvim-dap",
+		config = function(plugin, ...)
+			plugin._.super.config(...)
+			local dap = require("dap")
+			dap.adapters.netcoredbg = {
+				type = "executable",
+				command = "netcoredbg",
+				args = { "--interpreter=vscode" },
+			}
+			require("dap.ext.vscode").load_launchjs(nil, { coreclr = { "cs" } })
+		end,
+	},
+	{
 		"nvim-neotest/neotest",
 		opts = {
 			adapters = {
@@ -8,33 +21,6 @@ return {
 		},
 		dependencies = {
 			"Issafalcon/neotest-dotnet",
-			config = function(_, opts)
-				-- local install_dir = path.concat({ vim.fn.stdpath("data"), "mason" })
-				local install_dir = vim.fn.stdpath("data") .. "/" .. "mason"
-
-				local dap = require("dap")
-				dap.adapters.coreclr = {
-					type = "executable",
-					command = install_dir .. "/packages/netcoredbg/netcoredbg",
-					args = { "--interpreter=vscode" },
-				}
-
-				dap.adapters.netcoredbg = {
-					type = "executable",
-					command = install_dir .. "/packages/netcoredbg/netcoredbg",
-					args = { "--interpreter=vscode" },
-				}
-				dap.configurations.cs = {
-					{
-						type = "coreclr",
-						name = "launch - netcoredbg",
-						request = "launch",
-						program = function()
-							return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
-						end,
-					},
-				}
-			end,
 		},
 	},
 }
