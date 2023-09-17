bb95a925d05c6093557d627fc5dc8dcc0f1c3f5f
alex <alexnlarsen@gmail.com>
2023-09-17 14:05:38 +0200

    Disable annoying toggleterm binds

--- config/nvim/lua/plugins/term.lua
+++ config/nvim/lua/plugins/term.lua
@@ -3,19 +3,6 @@ return {
     "akinsho/toggleterm.nvim",
     cmd = "ToggleTerm",
     config = function(_, opts)
-      function _G.set_terminal_keymaps()
-        local keymap_opts = { noremap = false }
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-w>", [[<C-\><C-n>]], keymap_opts)
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-w>i", [[<C-\><C-n>]], keymap_opts)
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-w>c", [[<C-\><C-n><C-W>c]], keymap_opts)
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-w>h", [[<C-\><C-n><C-W>h]], keymap_opts)
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-w>j", [[<C-\><C-n><C-W>j]], keymap_opts)
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-w>k", [[<C-\><C-n><C-W>k]], keymap_opts)
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-w>l", [[<C-\><C-n><C-W>l]], keymap_opts)
-      end
-
-      vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
-
       local toggleterm = require("toggleterm")
       toggleterm.setup(opts)
     end,
