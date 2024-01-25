995eb0debff27a7cfae9af79f9faa1489d546681
alex <alexnlarsen@gmail.com>
2024-01-25 08:40:55 +0100

    Disable caps control hold

--- config/dwm/configurekeyboard.sh
+++ config/dwm/configurekeyboard.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-# map ctrl tab to escape
-xcape -t 280
-# map caps to ctrl
-setxkbmap us -variant altgr-intl -option ctrl:nocaps
+capscontrol() {
+	# map ctrl tab to escape
+	xcape -t 280
+	# map caps to ctrl
+	setxkbmap us -variant altgr-intl -option ctrl:nocaps
+}
+
+capsescape() {
+	setxkbmap us -variant altgr-intl -option caps:escape
+}
+
+
+capsescape
