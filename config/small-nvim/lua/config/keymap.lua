3b759fa16289f1a0e746e737898c9ae09edab2df
alex <alexnlarsen@gmail.com>
2024-01-27 13:40:33 +0100

    Add keybinds

--- config/small-nvim/lua/config/keymap.lua
+++ config/small-nvim/lua/config/keymap.lua
@@ -2,6 +2,15 @@ local opts = { silent = true, noremap = true }
 local function nmap(keys, action)
   vim.keymap.set("n", keys, action, opts)
 end
+
+local function vmap(keys, action)
+  vim.keymap.set("v", keys, action, opts)
+end
+
+local function imap(keys, action)
+  vim.keymap.set("i", keys, action, opts)
+end
+
 -- Insert blank line before/after cursor and restore position
 nmap("[<space>", "m'O<esc>`'")
 nmap("]<space>", "m'o<esc>`'")
@@ -10,6 +19,8 @@ nmap("=ip", "m`=ip``")
 -- reindent on paste
 nmap("p", "p`[v`]=")
 nmap("<S-p>", "P`[v`]=")
+-- visual select last paste
+nmap("gp", "`[v`]")
 nmap("<C-s>", ":w<cr>")
 nmap("<esc>", "<esc>:nohlsearch<cr>")
 nmap("<S-h>", ":bprev<cr>")
@@ -23,12 +34,17 @@ nmap("g?", function()
   end
 end)
 
-vim.keymap.set({ "v", "n" }, "<leader>cf", vim.lsp.buf.format)
-vim.keymap.set("v", "<M-j>", ":m '>+1<cr>gv=gv")
-vim.keymap.set("v", "<M-k>", ":m '<-2<cr>gv=gv")
-vim.keymap.set("n", "<M-j>", ":m .+1<cr>==")
-vim.keymap.set("n", "<M-k>", ":m .-2<cr>==")
+vim.keymap.set({ "v", "n" }, "<leader>cf", vim.lsp.buf.format, opts)
+vmap("<M-j>", ":m '>+1<cr>gv=gv")
+vmap("<M-k>", ":m '<-2<cr>gv=gv")
+nmap("<M-j>", ":m .+1<cr>==")
+nmap("<M-k>", ":m .-2<cr>==")
 
 -- create undo point before pasting
-vim.keymap.set("i", "<C-r>", "<C-G>u<C-r>", opts)
+imap("<C-r>", "<C-G>u<C-r>")
 
+-- keep visual selection when 'denting
+vmap("<", "<gv")
+vmap(">", ">gv")
+
+imap("", "<del>")
