d04cc5ea40470b39c1cced21c583716eaeb92ac9
alex <alexnlarsen@gmail.com>
2024-03-10 08:57:10 +0100

    Add overseer picker. Re-enable auto complete

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -72,6 +72,11 @@ require('lazy').setup({
       "stevearc/overseer.nvim",
       keys = {
         {
+          "<leader>rp",
+          "<cmd>OverseerLoadBundle<cr>",
+          desc = "Pick a saved task bundle",
+        },
+        {
           "<leader>rw",
           "<cmd>OverseerQuickAction watch<cr>",
           desc = "Rerun on save (Overseer)",
@@ -885,7 +890,6 @@ cmp.setup {
   },
   completion = {
     -- autocompletion while typing is distracting >:(
-    autocomplete = false,
     completeopt = 'menu,menuone,noinsert',
   },
   mapping = cmp.mapping.preset.insert {
