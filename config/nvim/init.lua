4180ec715923e7a861712f0094e27dd7cd9f4a84
alex <alexnlarsen@gmail.com>
2024-04-26 06:26:03 +0200

    Add bindings for quickfix navigation

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -464,6 +464,8 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '[q', ':cprev<cr>', { desc = 'Go to previous quickfix item' })
+vim.keymap.set('n', ']q', ':cnext<cr>', { desc = 'Go to next quickfix item' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
