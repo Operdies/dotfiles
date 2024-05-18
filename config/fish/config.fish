c6a304651c67fb40aa2d5a6d1dc7fae073189861
alex <alexnlarsen@gmail.com>
2024-05-18 09:54:07 +0200

    Add exports from zsh config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,28 +1,49 @@
-function ensure_fisher
-    set fish_src "$HOME/.config/fish/functions/fisher.fish"
-    # curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-    if [ ! -f "$fish_src" ]
-        curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-    end
+export LANG=en_US.UTF-8
+export DEBUGINFOD_URLS="https://debuginfod.archlinux.org/"
+export PATH="$HOME/.local/bin:$HOME/.cargo/bin:$PATH"
+export EDITOR='nvim'
+export FZF_DEFAULT_OPTS='--layout=reverse'
+export MANPAGER='nvim +Man! -'
 
-    set plugins "catppuccin/fish" \
-        "jorgebucaran/hydro" \
-        "jethrokuan/z"
-    set installed $(fisher list)
-    for plugin in $plugins
-        if not contains $plugin $installed
-            fisher install $plugin
-        end
+# don't nest nvim
+if [ -n "$NVIM" ]
+    if command -v nvr &> /dev/null
+        alias nvim="nvr -l"
+        export MANPAGER='nvr -l +Man! -'
+        export EDITOR='nvr -l'
     end
 end
 
-
 if status is-interactive
+    function ensure_fisher
+        set fish_src "$HOME/.config/fish/functions/fisher.fish"
+        # curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+        if [ ! -f "$fish_src" ]
+            curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+        end
+
+        set plugins "catppuccin/fish" \
+        "jorgebucaran/hydro" \
+        "jethrokuan/z" \
+        "2m/fish-history-merge"
+        set installed $(fisher list)
+        for plugin in $plugins
+            if not contains $plugin $installed
+                fisher install $plugin
+            end
+        end
+    end
+
     set --universal fish_color_error 'ff0000'
     set fish_greeting
-    # Commands to run in interactive sessions can go here
+
     ensure_fisher
     fish_config theme choose "Catppuccin Mocha"
     bind \ea execute history-search-backward
+
+    # startx on login on VT 1 if no display is set and the current session is on a tty
+    if [ -t 0 -a -z "$DISPLAY" -a "$XDG_VTNR" -eq 1 -a ]
+        startx
+    end
 end
 
