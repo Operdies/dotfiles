d35c7fe4d496b5aaa69a73fec924ea320a9b330f
alex <alexnlarsen@gmail.com>
2024-05-24 10:44:15 +0200

    Add powershell alias

---
+++ config/fish/conf.d/99-alias.fish
@@ -0,0 +1,5 @@
+if [ ! -z $WSLENV ]
+    alias psh='"/mnt/c/Program Files/PowerShell/7/pwsh.exe" -NoLogo -NoExit -WorkingDirectory C:/git'
+    alias pwsh='/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoLogo -NoExit -Command cd C:/git'
+end
+
