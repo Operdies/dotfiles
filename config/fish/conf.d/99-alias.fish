ddb4e8516ddedc8d9b3ddded1072484d3402fd4d
alex <alexnlarsen@gmail.com>
2024-05-24 21:53:03 +0200

    oops

--- config/fish/conf.d/99-alias.fish
+++ config/fish/conf.d/99-alias.fish
@@ -1,4 +1,4 @@
-if [ ! -z $WSLENV ]
+if [ ! -z "$WSLENV" ]
     alias psh='"/mnt/c/Program Files/PowerShell/7/pwsh.exe" -NoLogo -NoExit -WorkingDirectory C:/git'
     alias pwsh='/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoLogo -NoExit -Command cd C:/git'
 end
