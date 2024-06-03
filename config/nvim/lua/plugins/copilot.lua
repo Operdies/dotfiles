9331c2726449b4ed254231ddc9aaa02b629ff5ef
alex <alexnlarsen@gmail.com>
2024-06-03 19:30:47 +0200

    Disable copilot help

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -28,7 +28,7 @@ return {
       return {
         model = "gpt-4",
         auto_insert_mode = true,
-        show_help = true,
+        show_help = false,
         question_header = "  " .. user .. " ",
         answer_header = "  Copilot ",
         window = {
