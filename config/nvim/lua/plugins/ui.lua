43721b3c26a8c908cb675984ec802dc4b23c30b4
alex <alexnlarsen@gmail.com>
2024-06-23 10:50:27 +0200

    Cleanup bindings

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -131,7 +131,6 @@ return {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
     keys = {
-      { "<leader>bo", "<cmd>%bd|e#<cr>",              desc = "Delete other buffers" },
       { "<S-h>",      "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
       { "<S-l>",      "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
     },
