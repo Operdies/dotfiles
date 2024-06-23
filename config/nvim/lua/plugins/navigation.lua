d3cfdecf5728a47fec5d19d2e60a8876ae000059
alex <alexnlarsen@gmail.com>
2024-06-23 10:51:55 +0200

    Add hbac and leap

--- config/nvim/lua/plugins/navigation.lua
+++ config/nvim/lua/plugins/navigation.lua
@@ -37,4 +37,71 @@ return {
       },
     },
   },
+  {
+    'axkirillov/hbac.nvim',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+    },
+    event = "VeryLazy",
+    opts = {
+      autoclose                  = false,
+      threshold                  = 10,
+      close_command              = function(bufnr)
+        vim.api.nvim_buf_delete(bufnr, {})
+      end,
+      close_buffers_with_windows = false,
+    },
+    config = function(_, opts)
+      local hbac = require('hbac')
+      local actions = require("hbac.telescope.actions")
+
+      opts['telescope'] = {
+        sort_mru = true,
+        sort_lastused = true,
+        selection_strategy = "row",
+        use_default_mappings = false,
+        mappings = {
+          i = {
+            ["<C-q>"] = actions.close_unpinned,
+            ["<C-w>"] = actions.delete_buffer,
+            ["<C-e>"] = actions.toggle_pin,
+          },
+          n = {
+            ["<C-q>"] = actions.close_unpinned,
+            ["<C-w>"] = actions.delete_buffer,
+            ["<C-e>"] = actions.toggle_pin,
+          },
+        },
+        -- Pinned/unpinned icons and their hl groups. Defaults to nerdfont icons
+        pin_icons = {
+          pinned = { "󰐃 ", hl = "DiagnosticOk" },
+          unpinned = { "󰤱 ", hl = "DiagnosticError" },
+        },
+      }
+
+      hbac.setup(opts)
+      require('telescope').load_extension('hbac')
+
+      -- Overrides existing telescope.builtins.buffers bind with hbac variant
+      vim.keymap.set('n', '<leader>fb', "<cmd>Telescope hbac buffers<CR>", { desc = 'Select hbac buffer' })
+    end,
+  },
+  {
+    "ggandor/leap.nvim",
+    enabled = true,
+    keys = {
+      { "s",  mode = { "n", "x", "o" }, desc = "Leap Forward to" },
+      { "S",  mode = { "n", "x", "o" }, desc = "Leap Backward to" },
+      { "gs", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
+    },
+    config = function(_, opts)
+      local leap = require("leap")
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      vim.keymap.del({ "x", "o" }, "x")
+      vim.keymap.del({ "x", "o" }, "X")
+    end,
+  },
 }
