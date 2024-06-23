48b27cd3765233bf2bdea8752da3c639811741ff
alex <alexnlarsen@gmail.com>
2024-06-23 10:50:04 +0200

    Move whichkey

--- config/nvim/lua/plugins/gadgets.lua
+++ config/nvim/lua/plugins/gadgets.lua
@@ -2,4 +2,9 @@ return {
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim',  opts = {} },
   { 'echasnovski/mini.pairs', opts = {} },
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {},
+  },
 }
