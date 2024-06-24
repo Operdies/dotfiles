ced71ee4a76eab2fb236dd506553048edfc705a5
alex <alexnlarsen@gmail.com>
2024-06-24 13:23:56 +0200

    Update wsl check

--- config/fish/conf.d/99-alias.fish
+++ config/fish/conf.d/99-alias.fish
@@ -1,4 +1,4 @@
-if [ ! -z "$WSLENV" ]
+if [ -f /etc/wsl.conf ]
     alias pwsh='"/mnt/c/Program Files/PowerShell/7/pwsh.exe" -NoLogo -NoExit -WorkingDirectory C:/git'
     alias powershell='/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoLogo -NoExit -Command cd C:/git'
 end
