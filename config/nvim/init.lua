d59e6ad7763b53df3abfb47587157f9ccd1185d7
alex <alexnlarsen@gmail.com>
2024-06-25 16:00:17 +0200

    Update lualine config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -245,11 +245,53 @@ require('lazy').setup({
       -- See `:help lualine.txt`
       opts = {
         options = {
-          icons_enabled = false,
-          theme = 'onedark',
+          icons_enabled = true,
+          theme = 'auto',
           component_separators = '|',
           section_separators = '',
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 10000,
+            tabline = 10000,
+            winbar = 10000,
+          }
         },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            }
+          },
+          lualine_x = { 'encoding' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            }
+          },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {}
+        },
+        winbar = {},
+        tabline = {},
+        inactive_winbar = {},
+        extensions = { 'oil', 'lazy', 'overseer', 'nvim-dap-ui', 'trouble', 'toggleterm', 'quickfix', 'man' },
       },
     },
 
