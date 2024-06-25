19276d5c3f38b9d9d47db0ee84bfd0a4d9bc6edf
alex <alexnlarsen@gmail.com>
2024-06-25 16:00:01 +0200

    Add indentation autocmd for C# files

--- config/nvim/lua/config/autocmd.lua
+++ config/nvim/lua/config/autocmd.lua
@@ -11,6 +11,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 vim.api.nvim_create_autocmd('BufEnter', {
   group = vim.api.nvim_create_augroup("CSharpIndent", { clear = true }),
+  pattern = { "*.cs" },
+  callback = function()
+    vim.opt_local.filetype = "cs"
+    vim.opt_local.shiftwidth = 4
+  end,
+})
+
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  group = vim.api.nvim_create_augroup("XmlIndent", { clear = true }),
   pattern = { "*.csproj", "*.props" },
   callback = function()
     vim.opt_local.filetype = "xml"
