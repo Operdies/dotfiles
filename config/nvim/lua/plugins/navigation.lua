817a6a316eb7f63456f4a2e234cbcf2c8ad3ada9
alex <alexnlarsen@gmail.com>
2024-06-27 09:53:32 +0200

    Add dashboard + persistence

--- config/nvim/lua/plugins/navigation.lua
+++ config/nvim/lua/plugins/navigation.lua
@@ -108,7 +108,6 @@ return {
   },
   {
     "ahmedkhalf/project.nvim",
-    event = "VeryLazy",
     config = function(_, opts)
       require("project_nvim").setup(opts)
       require("telescope").load_extension("projects")
