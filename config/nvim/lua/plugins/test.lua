c1b62fbdd7d8b7291b4864a02e5931cf3a2c857c
alex <alexnlarsen@gmail.com>
2024-06-28 15:33:21 +0200

    Add unittesting keymaps

--- config/nvim/lua/plugins/test.lua
+++ config/nvim/lua/plugins/test.lua
@@ -4,6 +4,16 @@ return {
     dependencies = {
       "Issafalcon/neotest-dotnet",
     },
+    keys = {
+      { "<leader>uR", function() require('neotest').run.run_last() end, desc = "Run recent test" },
+      { "<leader>ud", function() require("neotest").run.run({ strategy = "dap" }) end,  desc = "Debug closest test" },
+      { "<leader>uD", function() require("neotest").run.run_last({ strategy = "dap" }) end,  desc = "Debug recent test" },
+      { "<leader>us", "<cmd>Neotest summary<cr>", desc = "Open test browser" },
+      { "<leader>ur", "<cmd>Neotest run<cr>", desc = "Run closest test" },
+      { "<leader>ua", "<cmd>Neotest stop<cr>",  desc = "Stop running test" },
+      { "[t", "<cmd>Neotest jump prev<cr>",  desc = "Go to previous test" },
+      { "]t", "<cmd>Neotest jump next<cr>",  desc = "Go to next test" },
+    },
     opts = function()
       return {
         adapters = {
