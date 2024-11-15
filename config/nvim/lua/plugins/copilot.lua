2c6e1013e70f203a880d2385e370a01e64af074d
alex <alexnlarsen@gmail.com>
2024-11-15 10:16:48 +0100

    update copilotchat plugin config

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -21,6 +21,11 @@ return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+    },
+    build = "make tiktoken",        -- Only on MacOS or Linux
     cmd = "CopilotChat",
     opts = function()
       local user = vim.env.USER or "User"
