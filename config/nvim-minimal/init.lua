f6c8b8bfbb41675e594ce0dbe458435ccb3b8cfe
alex <alexnlarsen@gmail.com>
2025-09-04 11:32:27 +0200

    mini.hipatterns: setup

--- config/nvim-minimal/init.lua
+++ config/nvim-minimal/init.lua
@@ -72,6 +72,25 @@ vim.pack.add({
 require('mini.ai').setup()
 --]]
 
+-- NOTE(#307090)
+--[[ mini.hipatterns ]]
+local hipatterns = require('mini.hipatterns')
+hipatterns.setup({
+  highlighters = {
+    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+    hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
+    todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+    note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+
+    -- Highlight hex color strings (`#rrggbb`) using that color
+    hex_color = hipatterns.gen_highlighter.hex_color(),
+  },
+})
+vim.keymap.set('n', '<leader>ft', function () require('mini.extra').pickers.hipatterns({ scope = 'all'}) end)
+--]]
+
+
 --[[ gitsigns ]]
 -- Adds git related signs to the gutter, as well as utilities for managing changes
 local gs = require('gitsigns')
