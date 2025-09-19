ddff00518d2e05d541724b0d77376964c5d19ee7
alex <alexnlarsen@gmail.com>
2025-09-19 10:55:34 +0200

    Pick: fix bind for Git files

--- config/nvim-minimal/init.lua
+++ config/nvim-minimal/init.lua
@@ -645,8 +645,8 @@ vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y') -- yank to system clipboard
 vim.keymap.set({ 'n', 'x' }, '<leader>d', '"+d') -- delete to system clipboard
 vim.keymap.set('n', '<C-s>', "<cmd>write<cr>")  -- write buffer if it has unsaved changes
 vim.keymap.set('n', '<leader>fh', "<cmd>Pick help<CR>")
-vim.keymap.set('n', '<leader>ff', "<cmd>Pick files tool=git<CR>")
-vim.keymap.set('n', '<leader>fF', "<cmd>Pick files tool=fd<CR>")
+vim.keymap.set('n', '<leader>ff', function() require('mini.pick').builtin.files({ tool = "fd" }, nil) end)
+vim.keymap.set('n', 'gff', function() require('mini.pick').builtin.files({ tool = "git" }, nil) end)
 vim.keymap.set('n', '<leader>fg', "<cmd>Pick grep_live<CR>")
 vim.keymap.set('n', '<leader>cs', "<cmd>Pick lsp scope='document_symbol'<cr>")
 vim.keymap.set('n', '<leader>cS', "<cmd>Pick lsp scope='workspace_symbol'<cr>")
