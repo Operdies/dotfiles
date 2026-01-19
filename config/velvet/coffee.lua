49bbb30519ee65e172e17410824b18ba90d2b02d
alex <alexnlarsen@gmail.com>
2026-01-19 20:14:09 +0100

    config: move toys to separate files

---
+++ config/velvet/coffee.lua
@@ -0,0 +1,123 @@
+local vv = require('velvet')
+
+-- Found at https://emojicombos.com/coffee-ascii-art
+local coffee_frames = { [[
+                  ⢀
+                  ⡼
+                 ⣼⠇
+                ⠰⡏
+                ⢰⡇
+                ⢸⣷⡀
+                 ⢿⣷⡀
+               ⢀ ⠈⢻⣿⣄
+               ⠈⢆  ⠙⣿⣆
+                 ⢧  ⠘⢿⣇
+                 ⣸⡆  ⠘⣿⡀
+                ⣰⣿⠃   ⣿⠇
+               ⣼⣿⠃    ⡿⠁
+               ⣿⡏⣀⣀⣀ ⡜
+       ⣀⡤⠤⠒⠒⠋⠉⠉⠻⣧   ⠈⠉⠁   ⠢⢄
+      ⣾⣿    ⣀⣀⣀⣀⣤⣽⣦⣄⣀⣀⣀⣀    ⢹
+      ⣿⣿⣿⠷⠾⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠶⠚
+      ⢿⣿⡏      ⠈⠉⠉⠉⠉⠉⠉       ⣸⠛⠻⣷
+      ⠸⣿⣧                   ⢀⠃ ⢠⣿⠇
+       ⣹⣿⡆                 ⢠⣎⣠⣴⠿⠃
+ ⢀⣠⠔⠒⠈⠉ ⠹⣿⣄               ⣠⠾⠛⠛⠉⠒⠢⣄
+ ⣿⡁      ⠈⢻⣦⡀           ⣀⣾⡃       ⡟
+ ⠙⠻⣶⣀      ⠈⠙⠲⠦⣤⣄⣀⣀⣀⣤⣤⣾⣯⡵⠞⠋   ⣀⠟
+    ⠉⠛⠻⠿⠿⠶⠶⠤⠤⠤⣄⣀⣀⣀⣀⣀⣀⣀⣀⡠⠤⠤⠤⠴⠖⠉
+]], [[
+                   ⢀
+                   ⡼
+                  ⣼⠇
+                 ⠰⡏
+                 ⢰⡇
+                 ⢸⣷⡀
+                  ⢿⣷⡀
+                ⢀ ⠈⢻⣿⣄
+                ⠈⢆  ⠙⣿⣆
+                  ⢧  ⠘⢿⣇
+                  ⣸⡆  ⠘⣿⡀
+                 ⣰⣿⠃   ⣿⠇
+                ⣼⣿⠃    ⡿⠁
+                ⣿⡏⣀⣀  ⡜
+       ⣀⡤⠤⠒⠒⠋⠉⠉⠉⠻⣧  ⠈⠉⠁   ⠢⢄
+      ⣾⣿    ⣀⣀⣀⣀⣤⣽⣦⣄⣀⣀⣀⣀    ⢹
+      ⣿⣿⣿⠷⠾⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠶⠚
+      ⢿⣿⡏      ⠈⠉⠉⠉⠉⠉⠉       ⣸⠛⠻⣷
+      ⠸⣿⣧                   ⢀⠃ ⢠⣿⠇
+       ⣹⣿⡆                 ⢠⣎⣠⣴⠿⠃
+ ⢀⣠⠔⠒⠈⠉ ⠹⣿⣄               ⣠⠾⠛⠛⠉⠒⠢⣄
+ ⣿⡁      ⠈⢻⣦⡀           ⣀⣾⡃       ⡟
+ ⠙⠻⣶⣀      ⠈⠙⠲⠦⣤⣄⣀⣀⣀⣤⣤⣾⣯⡵⠞⠋   ⣀⠟
+    ⠉⠛⠻⠿⠿⠶⠶⠤⠤⠤⣄⣀⣀⣀⣀⣀⣀⣀⣀⡠⠤⠤⠤⠴⠖⠉
+]] }
+local frame_order = { 2, 1 }
+
+-- some small number which most likely doesn't cover any windows
+local bg_z = -100000
+local bg = require('velvet.window').create()
+bg:set_z_index(bg_z)
+bg:set_opacity(0)
+bg:set_transparency_mode(vv.api.transparency_mode.all)
+bg:set_line_wrapping(false)
+local colors = { 'red', 'green', 'blue', 'white', 'magenta', 'yellow' }
+local color = 1
+local frame = 1
+
+local function draw_coffee()
+  frame = 1 + (frame % #frame_order)
+  local coffee = coffee_frames[frame_order[frame]]
+  local sz = vv.api.get_screen_geometry()
+  bg:set_geometry({ left = 0, top = 0, width = sz.width, height = sz.height })
+
+  bg:clear_background_color()
+  bg:set_foreground_color(colors[color])
+  bg:clear()
+  local width = 0
+  local height = 0
+  for line in coffee:gmatch('(.-)\n') do
+    local strwidth = utf8.len(line) or 0
+    if strwidth > width then width = strwidth end
+    height = height + 1
+  end
+
+  local lnum = 0
+  local col = (sz.width // 2) - (width // 2)
+  local row = sz.height // 2 - height // 2
+  if col < 0 then col = 0 end
+  if row < 0 then row = 0 end
+  for line in coffee:gmatch('(.-)\n') do
+    bg:set_cursor(col, row + lnum)
+    bg:draw(line)
+    lnum = lnum + 1
+    if lnum >= sz.height then break end
+  end
+end
+
+do
+  bg:on_mouse_click(function(_, args)
+    if args.mouse_button == vv.api.mouse_button.left and args.event_type == vv.api.mouse_event_type.mouse_down then
+      color = 1 + (color % #colors)
+      draw_coffee()
+    end
+  end)
+end
+
+return {
+  enable = function()
+    draw_coffee()
+    require('velvet.events').create_group('coffee_redraw', true).screen_resized = draw_coffee
+    local coffee_animation_schedule = nil
+    coffee_animation_schedule = function()
+      if bg:valid() then
+        draw_coffee()
+        vv.api.schedule_after(1500, coffee_animation_schedule)
+      end
+    end
+    coffee_animation_schedule()
+  end,
+  disable = function()
+  end
+}
+
---
+++ config/velvet/paint.lua
@@ -0,0 +1,130 @@
+local vv = require('velvet')
+local paint = {}
+local cool_win = nil
+function paint.create_paint()
+  local sz = vv.api.get_screen_geometry()
+  if cool_win then cool_win:close() end
+  cool_win = require('velvet.window').create()
+
+  local brush = {
+    red = 0,
+    green = 0,
+    blue = 0,
+  }
+  local opacity = 1
+
+  local width, height = 30, 12
+  cool_win:set_geometry({ left = sz.width - width - 15, top = 2, width = width, height = height })
+  cool_win:set_opacity(opacity)
+  cool_win:set_background_color('white')
+  cool_win:clear()
+  cool_win:set_background_color(brush)
+
+  local function update_brush()
+    cool_win:set_cursor(1, 1)
+    cool_win:set_background_color(brush)
+    cool_win:draw('brush')
+  end
+
+  cool_win:on_mouse_scroll(function(win, scroll)
+    if scroll.direction == vv.api.scroll_direction.down then
+      brush.red = (brush.red + 5) % 255
+    elseif scroll.direction == vv.api.scroll_direction.up then
+      brush.red = (brush.red - 5) % 255
+    elseif scroll.direction == vv.api.scroll_direction.left then
+      brush.green = (brush.green + 5) % 255
+    elseif scroll.direction == vv.api.scroll_direction.right then
+      brush.green = (brush.green - 5) % 255
+    end
+
+    local geom = win:get_geometry()
+    update_brush()
+  end)
+
+  do
+    local dragging = false
+    cool_win:on_mouse_click(function(win, click)
+      if click.mouse_button == vv.api.mouse_button.left then
+        dragging = click.event_type == vv.api.mouse_event_type.mouse_down
+      end
+    end)
+
+    cool_win:on_mouse_move(function(win, move)
+      if dragging then
+        win:set_cursor(move.pos.col, move.pos.row)
+        win:draw(' ')
+      end
+    end)
+  end
+
+  local close_sequence = '<C-x>closepaint'
+  map(close_sequence, function()
+    cool_win:close()
+    vv.api.keymap_del(close_sequence)
+  end)
+
+  local function index_to_color(i)
+    local geom = cool_win:get_geometry()
+    return math.floor(i * (255 / geom.height))
+  end
+
+  local function slider_update(slider, color, selected)
+    local geom = cool_win:get_geometry()
+    for i = 1, geom.height do
+      slider:set_cursor(1, i)
+      local bg = { red = 0, green = 0, blue = 0 }
+      bg[color] = index_to_color(i)
+      slider:set_background_color(bg)
+      if i == selected then
+        slider:draw(' ◇ ')
+      else
+        slider:draw('   ')
+      end
+    end
+  end
+
+  --- @return velvet.window
+  local function create_slider(color, offset)
+    local geom = cool_win:get_geometry()
+    local slider = cool_win:create_child_window()
+    slider:set_geometry({
+      left = geom.left + geom.width + offset,
+      width = 3,
+      height = geom.height,
+      top = geom.top,
+    })
+
+    slider_update(slider, color, 1)
+    return slider
+  end
+
+  local red_slider = create_slider('red', 0)
+  local green_slider = create_slider('green', 3)
+  local blue_slider = create_slider('blue', 6)
+
+  local sliders = {
+    red = red_slider,
+    green = green_slider,
+    blue = blue_slider
+  }
+
+  for scol, slider in pairs(sliders) do
+    local function set_color_index(i)
+      brush[scol] = index_to_color(i)
+      slider_update(slider, scol, i)
+      update_brush()
+    end
+    slider:on_mouse_click(function(_, args)
+      if args.mouse_button == vv.api.mouse_button.left then
+        set_color_index(args.pos.row)
+      end
+    end)
+    slider:on_mouse_move(function(_, args)
+      if args.mouse_button == vv.api.mouse_button.left then
+        set_color_index(args.pos.row)
+      end
+    end)
+  end
+end
+
+return paint
