9e6612f692312f241826ba0946cd77aae1e8e6ec
Alexander Larsen <alexander.larsen@keysight.com>
2026-03-27 14:44:04 +0100

    agent.sh: podman fix and general improvements
    * terminate flag passing after '--'
    * factor out docker block in function
    * slightly safer .ssh/ deny rules
    * podman compatibility fix (.ssh/known_hosts read)
    * more explanatory comments

--- agent.sh
+++ agent.sh
@@ -8,7 +8,7 @@
 # sandboxing reference: https://reverse.put.as/wp-content/uploads/2011/09/Apple-Sandbox-Guide-v1.0.pdf
 
 # debug permissions with:
-# Sudo log stream --predicate 'process == "kernel" AND eventMessage CONTAINS "Sandbox"'
+# sudo log stream --predicate 'process == "kernel" AND eventMessage CONTAINS "Sandbox"'
 
 # Prevent dotnet from using a compilation server.
 # This causes weird issues when compilation servers are shared between agents.
@@ -19,6 +19,11 @@ sandbox_config="code-mode"
 while ! test -z "$1"; do
   case "$1" in
     "")
+      ;;
+    "--")
+      shift
+      cmd=("$@")
+      break
       ;;
     "--mode")
       shift
@@ -47,43 +52,54 @@ while ! test -z "$1"; do
   shift
 done
 
+block-docker() {
+  cat << EOF
+(deny network*                                             ; block docker access since this is a trivial root sandbox escape
+  (literal "/var/run/docker.sock")
+  (literal "/private/var/run/docker.sock")
+  (literal "${HOME}/.docker/run/docker.sock"))
+EOF
+}
+
 chat-mode() {
   binary="$(realpath $(command which ${cmd[0]}))"
   workdir="$(pwd)"
   cat << EOF
 (version 1)
-(allow default)                             ; by default, allow everything.
-                                            ; It would be better to deny everything by default,
-                                            ; and explicitly enable required features,
-                                            ; but that is not really practical.
+(allow default)                                            ; by default, allow everything.
+                                                           ; It would be better to deny everything by default,
+                                                           ; and explicitly enable required features,
+                                                           ; but that is not really practical.
 
-(deny mach-lookup                           ; deny system resources, such as the keyring. (gh auth)
-  (global-name "com.apple.SecurityServer")) ; keyring
+(deny mach-lookup                                          ; deny system resources, such as the keyring. (gh auth)
+  (global-name "com.apple.SecurityServer"))                ; keyring. This is used by claude-code, but it has fallback handling after logging in without keyring access.
 
 
-(deny file-write* (regex "^/"))             ; deny writing all files by default.
+(deny file-write* (regex "^/"))                            ; deny writing all files by default.
 
 (allow file-write*  file-read*
   (regex "^/dev")
-  (regex "^/var")                           ; temp files
-  (regex "^/tmp")                           ; temp files
-  (regex "^/private")                       ; temp files
+  (regex "^/var")                                          ; temp files
+  (regex "^/tmp")                                          ; temp files
+  (regex "^/private")                                      ; temp files
 
-  (regex "^${HOME}/.claude")                ; claude config
-  (regex "^${HOME}/.claude.json")           ;
+  (regex "^${HOME}/.claude")                               ; claude config
+  (regex "^${HOME}/.claude.json")                          ;
 
-  (regex "^${HOME}/.local/share/opencode")  ; opencode config
-  (regex "^${HOME}/.local/state/opencode")  ;
-  (regex "^${HOME}/.config/opencode")       ;
-  (regex "^${HOME}/.cache/opencode"))       ;
+  (regex "^${HOME}/.local/share/opencode")                 ; opencode config
+  (regex "^${HOME}/.local/state/opencode")                 ;
+  (regex "^${HOME}/.config/opencode")                      ;
+  (regex "^${HOME}/.cache/opencode"))                      ;
 (allow file-read* (regex "^${workdir}"))
-
-(deny network*                              ; block docker access since this is a trivial full sandbox escape
-  (literal "/var/run/docker.sock")
-  (literal "/private/var/run/docker.sock")
-  (literal "${HOME}/.docker/run/docker.sock"))
-(deny file* (regex "^${HOME}/.ssh"))        ; deny reading / writing ssh keys
+; rule merging is a bit weird. If two rules match similar patterns,
+; presedence is sometimes decided by granularity; for example file-read* is more specific than file*, so file-read* might win.
+; to reduce the risk other rules add read in .ssh, we explicitly deny read* and write* in addition to file*
+(deny file* (regex "^${HOME}/.ssh"))                       ; deny reading / writing ssh keys
+(deny file-read* (regex "^${HOME}/.ssh"))                  ; deny reading / writing ssh keys
+(deny file-write* (regex "^${HOME}/.ssh"))                 ; deny reading / writing ssh keys
+(allow file-read* (literal "${HOME}/.ssh/known_hosts"))    ; podman needs read known hosts to function
 EOF
+  block-docker
 }
 
 code-mode() {
@@ -91,9 +107,9 @@ code-mode() {
   chat-mode
   cat << EOF
 (allow file-write* 
-  (regex "^${HOME}/.nuget")                 ; nuget cache
+  (regex "^${HOME}/.nuget")                 ; nuget cache. This is a potential attack vector since nuget packages can later be executed outside the sandbox, but it is impractical to deny.
   (regex "^${workdir}"))
 EOF
 }
 
-sandbox-exec -f <(${sandbox_config}) ${cmd[@]}
+exec sandbox-exec -f <(${sandbox_config}) ${cmd[@]}
