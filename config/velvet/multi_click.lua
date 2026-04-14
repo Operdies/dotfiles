89d3d4d36872e225be7d49afb590124ac09abb68
Alexander Larsen <alexander.larsen@keysight.com>
2026-04-14 10:16:40 +0200

    vv multiclick: cleanup

--- config/velvet/multi_click.lua
+++ config/velvet/multi_click.lua
@@ -1,41 +1,22 @@
---- @class multi_click.events
---- @field left_single_click velvet.async.event
---- @field left_double_click velvet.async.event
---- @field left_triple_click velvet.async.event
-
---- @type multi_click.events
 local M = {
-  ---@diagnostic disable-next-line: assign-type-mismatch
   left_single_click = 'mouse.left.click',
-  ---@diagnostic disable-next-line: assign-type-mismatch
   left_double_click = 'mouse.left.double_click',
-  ---@diagnostic disable-next-line: assign-type-mismatch
   left_triple_click = 'mouse.left.triple_click'
 }
 
 vv.async.run(function()
-  vv.async.run(function()
-    while true do
-      local clk = vv.async.wait_for_mouse_click()
-      if clk.event_type == 'mouse_down' and clk.mouse_button == 'left' then
-        vv.events.emit_event(M.left_single_click, clk)
-      end
-    end
-  end)
-
   local timeout = 500
-  local when = function(_, clk) return clk.event_type == 'mouse_down' and clk.mouse_button == 'left' end
+  local when = function(_, _, clk) return clk.event_type == 'mouse_down' and clk.mouse_button == 'left' end
   local left_click = { event = 'mouse.click', when = when }
   while true do
     local evt, double, triple
-    -- click
-    vv.async.wait_for_mouse_click(when, nil)
-    vv.events.emit_event(M.left_single_click, double)
-    evt, double = vv.async.wait(left_click, 'mouse.move', timeout)
-    if evt == left_click.event then
+    local _, _, single = vv.async.wait(left_click)
+    vv.events.emit_event(M.left_single_click, single)
+    evt, _, double = vv.async.wait(left_click, 'mouse.move', timeout)
+    if evt == left_click then
       vv.events.emit_event(M.left_double_click, double)
-      evt, triple = vv.async.wait(left_click, 'mouse.move', timeout)
-      if evt == left_click.event then
+      evt, _, triple = vv.async.wait(left_click, 'mouse.move', timeout)
+      if evt == left_click then
         vv.events.emit_event(M.left_triple_click, triple)
       else
       end
@@ -43,4 +24,10 @@ vv.async.run(function()
   end
 end)
 
+--- @class multi_click.events
+--- @field left_single_click velvet.async.event_registration
+--- @field left_double_click velvet.async.event_registration
+--- @field left_triple_click velvet.async.event_registration
+
+--- @cast M multi_click.events
 return M
