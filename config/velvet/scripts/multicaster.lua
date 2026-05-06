7e26ade6f49e56ac7ae6658dc45463deccccad6c
Alexander Larsen <alexander.larsen@keysight.com>
2026-05-06 10:05:22 +0200

    velvet: remove gc test
    it works now yay

--- config/velvet/scripts/multicaster.lua
+++ config/velvet/scripts/multicaster.lua
@@ -1,64 +1,27 @@
-collectgarbage()
-print('start')
-collectgarbage()
-local weakref = setmetatable({}, { __mode = 'kv' })
-weakref[1] = vv.async.run(function()
-  for i=1,10 do
-    collectgarbage()
-    print("inner", i)
-    vv.async.wait(10)
+local proc = arg[1]
+local cmd = arg[2]
+
+local threads = {}
+local this = vv.api.get_servername()
+for _, server in ipairs(vv.api.get_servernames()) do
+  if server ~= this then
+    print(string.format("Running %s[%s] on %s", proc, cmd, server))
+    local a = vv.async.run(function()
+      local remote = require('remote_call').create_remote_api(server)
+      local remote_window = remote.window_create_process(proc)
+      remote.window_send_keys(remote_window, cmd .. "\n")
+      vv.async.wait(1000)
+      remote.window_send_keys(remote_window, "<C-d>")
+    end)
+    threads[#threads+1] = a
   end
-end)
-collectgarbage()
-vv.async.wait(weakref[1])
-collectgarbage()
-print('end')
-collectgarbage()
+end
 
--- local proc = arg[1]
--- local cmd = arg[2]
---
--- local threads = {}
--- local this = vv.api.get_servername()
--- for _, server in ipairs(vv.api.get_servernames()) do
---   if server ~= this then
---     print(string.format("Running %s[%s] on %s", proc, cmd, server))
---     local a = vv.async.run(function()
---       local remote = require('remote_call').create_remote_api(server)
---       local remote_window = remote.window_create_process(proc)
---       remote.window_send_keys(remote_window, cmd .. "\n")
---       vv.async.wait(1000)
---       remote.window_send_keys(remote_window, "<C-d>")
---     end)
---     threads[#threads+1] = a
---   end
--- end
---
--- local function wait_all(args)
---   while #args > 0 do
---     local resolved = vv.async.wait(table.unpack(args))
---     table.remove_if(args, function(x) return x == resolved end)
---   end
--- end
---
--- wait_all(threads)
+local function wait_all(args)
+  while #args > 0 do
+    local resolved = vv.async.wait(table.unpack(args))
+    table.remove_if(args, function(x) return x == resolved end)
+  end
+end
 
-print('weak test')
-local a = {}
-local b = {}
-setmetatable(a, b)
-b.__mode = "kv"         -- now `a' has weak keys
-local key = vv.async.run(function() vv.async.wait('**') end)
-a[key] = 1
-key = vv.async.run(function()  end)
-a[key] = 2
-key = vv.async.run(function() vv.async.wait('**') end)
-a[key] = 3
-collectgarbage()       -- forces a garbage collection cycle
-for k, v in pairs(a) do print(v) end
-print('wait')
-vv.async.wait(10)
-print('wait done')
-collectgarbage()       -- forces a garbage collection cycle
-for k, v in pairs(a) do print(v) end
-print('weak test end')
+wait_all(threads)
