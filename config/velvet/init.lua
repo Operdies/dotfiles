3e7891a571569b4aacc71b443725293bdcf2ee44
Alexander Larsen <alexander.larsen@keysight.com>
2026-07-28 16:04:42 +0200

    velvet: move pickers to separate folder

--- config/velvet/init.lua
+++ config/velvet/init.lua
@@ -38,90 +38,28 @@ do
 end
 
 
-local velvet_window = require('velvet.window')
-
-map(map_prefix .. 'w', function()
-  local visible_indicator = "(*) "
-  local not_visible_indicator = "    "
-  local pick = require('velvet.pick')
-  local lst = vv.api.get_windows()
-  table.sort(lst, function(a, b) return a < b end)
-  local items = {}
-  local initial_index = -1
-  local current_focus = vv.api.get_focused_window()
-  for _, id in ipairs(lst) do
-    local w = velvet_window.from_handle(id)
-    if not w:is_lua() and not w:get_parent() then
-      local display = w:get_friendly_title()
-      local prefix = w:get_visibility() and visible_indicator or not_visible_indicator
-      items[#items + 1] = { text = prefix .. display, win = w }
-      if id == current_focus then initial_index = #items end
-    end
-  end
-
-  local underlay = nil
-  local prev_preview = { win = nil, z = nil, visible = nil, alpha = nil }
-  local function restore_prev()
-    local w = prev_preview and prev_preview.win
-    if w and w:valid() then
-      w:set_z_index(prev_preview.z)
-      w:set_visibility(prev_preview.visible)
-      w:set_alpha(prev_preview.alpha)
-    end
-  end
-  local function dispose()
-    restore_prev()
-    if underlay and underlay.close then underlay:close() end
-  end
-  pick.select(items, {
-    on_preview = function(sel)
-      local picker = pick.get_active_picker()
-      if not underlay then
-        underlay = picker:create_child_window()
-        underlay:set_z_index(picker:get_z_index() - 2)
-      end
-      local ts = vv.api.get_screen_geometry()
-      underlay:set_geometry({left = 1, top = 1, width = ts.width, height = ts.height })
-      underlay:set_background_color(vv.options.theme.background)
-      underlay:clear()
-      restore_prev()
-      prev_preview = { win = sel.win, z = sel.win:get_z_index(), visible = sel.win:get_visibility(), alpha = sel.win:get_alpha() }
-      sel.win:set_z_index(picker:get_z_index() - 1)
-      sel.win:set_visibility(true)
-      sel.win:set_alpha(1.0)
-      if sel.win.borders then sel.win:set_frame_color('magenta') end
-    end,
-    on_cancel = function()
-      dispose()
-    end,
-    on_choice = function(sel)
-      sel.win:focus()
-      dwm.make_visible(sel.win)
-      dispose()
-    end,
-    prompt = "Focus window: ",
-    initial_selection = initial_index,
-  })
-end, "Start window picker")
+map(map_prefix .. 'w', function() require('pickers.window').pick() end, "Start window picker")
 
 
 -- I am too used to the position of these keys on MacOS
 keymap:remap_key('§', '`')
 keymap:remap_key('±', '~')
 
-local quake = require('velvet.extras.quake')
-local quake1 = quake.create('zsh', 'default')
-local lazygit = quake.create('lazygit', 'lazygit')
-map(map_prefix .. "<C-\\>", quake1.toggle, "Toggle Zsh Quake")
-map(map_prefix .. "<C-]>", lazygit.toggle, "Toggle Lazygit Quake")
+do
+  local quake = require('velvet.extras.quake')
+  local quake_shell = quake.create('zsh', 'default')
+  local quake_lazygit = quake.create('lazygit', 'lazygit')
+  map(map_prefix .. "<C-\\>", quake_shell.toggle, "Toggle Zsh Quake")
+  map(map_prefix .. "<C-]>", quake_lazygit.toggle, "Toggle Lazygit Quake")
+end
 
 
+map(map_prefix .. 's', function() require('pickers.session').pick_session() end, "Switch session")
 map("<M-`>", dwm.select_previous_view, { description = "Select the previous view" })
 
 
 local log_connected = require('cli-logger').on_logger_connected
 
-map(map_prefix .. 's', require('pick_session').pick_session, "Switch session")
 
 require('catppuccin_mocha')
 
---
+++ config/velvet/pickers/window.lua
@@ -0,0 +1,71 @@
+local M = {}
+
+local velvet_window = require('velvet.window')
+local dwm = require('velvet.layout.dwm')
+
+function M.pick()
+  local visible_indicator = "(*) "
+  local not_visible_indicator = "    "
+  local pick = require('velvet.pick')
+  local lst = vv.api.get_windows()
+  table.sort(lst, function(a, b) return a < b end)
+  local items = {}
+  local initial_index = -1
+  local current_focus = vv.api.get_focused_window()
+  for _, id in ipairs(lst) do
+    local w = velvet_window.from_handle(id)
+    if not w:is_lua() and not w:get_parent() then
+      local display = w:get_friendly_title()
+      local prefix = w:get_visibility() and visible_indicator or not_visible_indicator
+      items[#items + 1] = { text = prefix .. display, win = w }
+      if id == current_focus then initial_index = #items end
+    end
+  end
+
+  local underlay = nil
+  local prev_preview = { win = nil, z = nil, visible = nil, alpha = nil }
+  local function restore_prev()
+    local w = prev_preview and prev_preview.win
+    if w and w:valid() then
+      w:set_z_index(prev_preview.z)
+      w:set_visibility(prev_preview.visible)
+      w:set_alpha(prev_preview.alpha)
+    end
+  end
+  local function dispose()
+    restore_prev()
+    if underlay and underlay.close then underlay:close() end
+  end
+  pick.select(items, {
+    on_preview = function(sel)
+      local picker = pick.get_active_picker()
+      if not underlay then
+        underlay = picker:create_child_window()
+        underlay:set_z_index(picker:get_z_index() - 2)
+      end
+      local ts = vv.api.get_screen_geometry()
+      underlay:set_geometry({ left = 1, top = 1, width = ts.width, height = ts.height })
+      underlay:set_background_color(vv.options.theme.background)
+      underlay:clear()
+      restore_prev()
+      prev_preview = { win = sel.win, z = sel.win:get_z_index(), visible = sel.win:get_visibility(), alpha = sel.win
+      :get_alpha() }
+      sel.win:set_z_index(picker:get_z_index() - 1)
+      sel.win:set_visibility(true)
+      sel.win:set_alpha(1.0)
+      if sel.win.borders then sel.win:set_frame_color('magenta') end
+    end,
+    on_cancel = function()
+      dispose()
+    end,
+    on_choice = function(sel)
+      sel.win:focus()
+      dwm.make_visible(sel.win)
+      dispose()
+    end,
+    prompt = "Focus window: ",
+    initial_selection = initial_index,
+  })
+end
+
+return M
